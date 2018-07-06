0a5af1412822591c35227888f07ced964b6fe74a
fmoronzirfas <fabian.moron-zirfas@ixds.com>
2018-07-06 13:11:59 +0200

    test(module test): fixed a manual module test

--- test/manual/as-module/index.js
+++ test/manual/as-module/index.js
@@ -1,9 +1,10 @@
-const {wsBroadcast} = require('../../lib/index');
+const {broadcast} = require('../../../lib/index');
 const path = require('path');
 
-const wss = wsBroadcast({
-  host: '0.0.0.0',
-  port: 3334,
-  path: '/',
-  exclusiveBroadcast: false
-});
+const wss = broadcast(
+//   {
+//   // host: '0.0.0.0',
+//   port: 7777,
+//   exclusiveBroadcast: false
+// }
+);
